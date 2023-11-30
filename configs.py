d2dcce2c5918e03a718bd1c7d682533958f7aa73
hiRahulbishts <152518853+hiRahulbishts@users.noreply.github.com>
2023-11-30 21:38:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,18 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
+  API_ID = int(os.environ.get("API_ID", "1615127"))
+  API_HASH = os.environ.get("API_HASH", "566474478c929ad4bd441c0bd269531c")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6318429756:AAHKYzltAqO9K2pjG2739liar-4k_eBGOmU
+")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Shorturlsxyzbot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002100631240"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "shorturls.zyx")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "712573408"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Zoroxtobot:dhj0BqA8i3LdxAM2@zorox.u8fbufy.mongodb.net/?retryWrites=true&w=majority")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001997021949")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002093432457"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
